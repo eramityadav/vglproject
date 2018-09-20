beff052eba4e45b780289cd66d0db32047f1cf6d
eramityadav <43442375+eramityadav@users.noreply.github.com>
2018-09-20 18:47:53 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        goto label1;
+
+        int target = -5;
+        int num = 3;
+
+        target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+        target = +num; // Noncompliant; target = 3
+
+        label1:
+        int i = 0;
+        int j = 0;
+        int k;
+        //k = i / j;
+
+
+    }
+}
